e89b0cde7831fbe86e0615c259be7bc77c3ad0c5
Jose Blanco <jdblancoes@gmail.com>
2021-05-04 20:19:29 +0100

    added cloudinary and change banner for list to the main pic

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,6 +25,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :imgsrc)
+    params.require(:list).permit(:name, :imgsrc, :picture)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ApplicationRecord
+  has_one_attached :picture
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :list_reviews
