6e4ba99fdeeecfa2958fbaf94f360a067622ff9f
Jose Blanco <jdblancoes@gmail.com>
2021-05-06 18:52:48 +0100

    added dependent destroy for list_reviews

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   has_one_attached :picture
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
-  has_many :list_reviews
+  has_many :list_reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
 end
